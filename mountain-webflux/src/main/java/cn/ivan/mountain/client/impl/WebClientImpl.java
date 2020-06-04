101a3ffef2eb4522586d819f7410c6822497784c
yanqi69 <yanqi69@jd.com>
2020-06-04 19:02:39 +0800

    init

--- mountain-webflux/src/main/java/cn/ivan/mountain/client/impl/WebClientImpl.java
+++ mountain-webflux/src/main/java/cn/ivan/mountain/client/impl/WebClientImpl.java
@@ -3,6 +3,8 @@ package cn.ivan.mountain.client.impl;
 import cn.ivan.mountain.bean.ApiMetadata;
 import cn.ivan.mountain.bean.MethodMetadata;
 import cn.ivan.mountain.client.MountainClient;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -10,6 +12,7 @@ import reactor.core.publisher.Mono;
  * @author yanqi69
  * @date 2020/6/3
  */
+@Slf4j
 public class WebClientImpl implements MountainClient {
 
     private WebClient webClient;
@@ -21,7 +24,11 @@ public class WebClientImpl implements MountainClient {
      */
     @Override
     public void init(ApiMetadata apiMetadata) {
-        this.webClient = WebClient.create(apiMetadata.getBaseUrl());
+        log.info("web client init ...");
+        this.webClient = WebClient.builder().baseUrl(apiMetadata.getBaseUrl()).filter(((request, next) -> {
+            log.info("{} HTTP {} {}", request.logPrefix(), request.method(), request.url());
+            return next.exchange(request);
+        })).build();
     }
 
     /**
@@ -32,21 +39,29 @@ public class WebClientImpl implements MountainClient {
      */
     @Override
     public Object invoke(MethodMetadata methodMetadata) {
+        log.debug("request params : {} ", JSON.toJSONString(methodMetadata));
         WebClient.RequestBodySpec requestBodySpec = webClient.method(methodMetadata.getMethod())
                 .uri(methodMetadata.getUri(), methodMetadata.getArgs());
         Mono body = methodMetadata.getBody();
         WebClient.ResponseSpec retrieve;
-        if(body != null){
+        if (body != null) {
             retrieve = requestBodySpec.bodyValue(body).retrieve();
-        }else {
+        } else {
             retrieve = requestBodySpec.retrieve();
         }
         Object result;
-        if(methodMetadata.isFlux()){
-            result = retrieve.bodyToFlux(methodMetadata.getReturnActualType());
-        }else {
-            result = retrieve.bodyToMono(methodMetadata.getReturnActualType());
+        if (methodMetadata.isFlux()) {
+            result = retrieve.bodyToFlux(methodMetadata.getReturnActualType()).filter(p -> {
+                log.debug("response flux:{}", JSON.toJSONString(p));
+                return true;
+            });
+        } else {
+            result = retrieve.bodyToMono(methodMetadata.getReturnActualType()).filter(p -> {
+                log.debug("response mono:{}", JSON.toJSONString(p));
+                return true;
+            });
         }
+        log.debug("build request success");
         return result;
     }
 }
